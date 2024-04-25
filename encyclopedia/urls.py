2de13421c0e8e34485693cc95c7b64a85218609a
simar-s2 <simarjot2468@gmail.com>
2024-04-25 23:57:56 +0300

    Add feature to allow users to search for entry through search bar and add links to pages in index.html

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path('', views.index, name="index"),
+    path('wiki/<title>', views.get_entry, name="get_entry")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -8,3 +8,7 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def get_entry(request, title):
+    return render(request, "encyclopedia/entry.html", {
+        "entry": util.get_entry(title)
+    })
