c5064c4af8a4cf1f2b96ec3b1b8352124acd41a4
simar-s2 <simarjot2468@gmail.com>
2024-04-26 04:10:38 +0300

    Add random entry selector

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('wiki/<title>', views.get_entry, name="get_entry"),
-    path('search', views.search, name="search")
+    path('search', views.search, name="search"),
+    path('random', views.random_selector, name="random_selector")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponseRedirect
 import difflib
+import random
 
 from . import util
 
 
 def index(request):
-        return render(request, "encyclopedia/index.html", {
-            "entries": util.list_entries(),
-        })
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries(),
+    })
 
 def get_entry(request, title):
     return render(request, "encyclopedia/entry.html", {
@@ -18,19 +19,19 @@ def get_entry(request, title):
 def search(request):
     if request.method == 'POST':
         query = request.POST.get('q')
-        if not query:
-            return HttpResponseRedirect("/")
-        elif util.get_entry(query) != None:
-            return HttpResponseRedirect(f"/wiki/{query}")
+        if not query: return HttpResponseRedirect("/")
+        elif util.get_entry(query) != None: return HttpResponseRedirect(f"/wiki/{query}")
         else:
             results = []
             entries = util.list_entries()
             for entry in entries:
                 similarity = difflib.SequenceMatcher(None, query, entry).ratio()
-                if similarity > 0.5:
-                    results.append(entry)
-            
+                if similarity > 0.5: results.append(entry)
+
     return render(request, "encyclopedia/search.html", {
         "results": results,
         "query": query
     })
+
+def random_selector(request): 
+    return HttpResponseRedirect(f'wiki/{random.choice(util.list_entries())}')
